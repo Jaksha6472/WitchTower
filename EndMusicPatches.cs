c8e1b4076e0587a9ceccaff03d55ef033be656e9
Jaksha6472 <freaky_chick@gmx.de>
2023-06-07 20:03:39 +0200

    first release

---
+++ EndMusicPatches.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using StardewValley;
+
+namespace AlvadeasWitchTower
+{
+    internal static class EndMusicPatches
+    {
+        public static void Apply(Harmony harmony)
+        {
+            harmony.Patch(
+                original: AccessTools.Method(typeof(GameLocation), nameof(GameLocation.cleanupBeforePlayerExit)),
+                prefix: new HarmonyMethod(typeof(EndMusicPatches), nameof(EndMusicPatches.Prefix_CleanupBeforePlayerExit))
+            );
+
+        }
+
+        private static void Prefix_CleanupBeforePlayerExit(GameLocation __instance)
+        {
+            if (Game1.currentLocation.NameOrUniqueName == "Custom_WalWitchtower")
+            {
+                Game1.changeMusicTrack("none", music_context: Game1.MusicContext.Default);
+            }
+
+            if (Game1.currentLocation.NameOrUniqueName == "Custom_WalWitchtowerCave")
+            {
+                Game1.changeMusicTrack("none", music_context: Game1.MusicContext.Default);
+            }
+        }
+    }
+}
